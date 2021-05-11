9d7f31d3de69eaa2c2b00209c05f341f3a1c5b17
John Manning <john.d.manning@gmail.com>
2021-05-11 00:28:30 -0400

    feat: add GetX counter to counter sandbox

---
+++ lib/ui/views/counter_sandbox/counter_sandbox_controller.dart
@@ -0,0 +1,5 @@
+import 'package:get/get.dart';
+
+class CounterSandboxController extends GetxController {
+  final counter = 0.obs;
+}
--- lib/ui/views/counter_sandbox/counter_sandbox_view.dart
+++ lib/ui/views/counter_sandbox/counter_sandbox_view.dart
@@ -1,11 +1,30 @@
 import 'package:amia_flutter_103/ui/styled_components/styled_components.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'counter_sandbox_controller.dart';
 
 class CounterSandboxView extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: StyledAppBar(context, title: 'Counter Sandbox'),
+    return GetX<CounterSandboxController>(
+      init: CounterSandboxController(),
+      builder: (controller) {
+        return Scaffold(
+          appBar: StyledAppBar(context, title: 'Counter Sandbox'),
+          body: Center(
+            child: Text(
+              'Button Pressed:\n${controller.counter} times',
+              textAlign: TextAlign.center,
+              style: Theme.of(context).textTheme.headline5,
+            ),
+          ),
+          floatingActionButton: FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () => controller.counter.value++,
+          ),
+        );
+      },
     );
   }
 }
