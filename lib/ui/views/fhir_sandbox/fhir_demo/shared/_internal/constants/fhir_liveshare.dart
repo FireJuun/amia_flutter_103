b6e6043e90e836495fcc5ff628715dc85054008f
John Manning <john.d.manning@gmail.com>
2021-05-18 14:44:30 -0400

    add FHIR puns to sandbox bug: for now, only works on first load... hot restart to load survey again again (until bug is fixed)

--- lib/ui/views/fhir_sandbox/fhir_demo/shared/_internal/constants/fhir_liveshare.dart
+++ lib/ui/views/fhir_sandbox/fhir_demo/shared/_internal/constants/fhir_liveshare.dart
@@ -35,8 +35,95 @@ const fhirLiveshareJson = {
               "valueCoding": {
                 "code": "1",
                 "display": "Welcome to the FHIR brigade"
+              },
+            },
+            {
+              "valueCoding": {"code": "2", "display": "The roof is on FHIR"}
+            },
+            {
+              "valueCoding": {
+                "code": "3",
+                "display": "Welcome to the FHIR brigade"
               }
-            }
+            },
+            {
+              "valueCoding": {"code": "4", "display": "FHIR 🔥 and Ice"}
+            },
+            {
+              "valueCoding": {"code": "5", "display": "FHIR-side chat"}
+            },
+            {
+              "valueCoding": {"code": "6", "display": "FHIR and Rain"}
+            },
+            {
+              "valueCoding": {"code": "7", "display": "Where's the FHIR?"}
+            },
+            {
+              "valueCoding": {"code": "8", "display": "the FHIRing squad"}
+            },
+            {
+              "valueCoding": {"code": "9", "display": "Fight FHIR with FHIR"}
+            },
+            {
+              "valueCoding": {
+                "code": "10",
+                "display": "Call code red, this place is on FHIR"
+              }
+            },
+            {
+              "valueCoding": {"code": "11", "display": "FHIRTruck"}
+            },
+            {
+              "valueCoding": {
+                "code": "12",
+                "display": "Dr. REDSTAT to ED for a FHIR 🔥!"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "13",
+                "display": "I'm the FHIRstarter, twisted FHIRstarter"
+              }
+            },
+            {
+              "valueCoding": {"code": "14", "display": "I got FHIR'd"}
+            },
+            {
+              "valueCoding": {
+                "code": "15",
+                "display": "Liar, Liar, Pants on FHIR"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "16",
+                "display": "Where there's smoke, there's FHIR"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "17",
+                "display": "FHIRing on all cylinders"
+              }
+            },
+            {
+              "valueCoding": {"code": "18", "display": "FHIRwall"}
+            },
+            {
+              "valueCoding": {"code": "19", "display": "Great Balls of FHIR"}
+            },
+            {
+              "valueCoding": {
+                "code": "20",
+                "display": "We don't need no water, let the FHIR burn"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "21",
+                "display": "We didn't start the FHIR"
+              }
+            },
           ]
         },
         {
@@ -66,8 +153,95 @@ const fhirLiveshareJson = {
               "valueCoding": {
                 "code": "1",
                 "display": "Welcome to the FHIR brigade"
+              },
+            },
+            {
+              "valueCoding": {"code": "2", "display": "The roof is on FHIR"}
+            },
+            {
+              "valueCoding": {
+                "code": "3",
+                "display": "Welcome to the FHIR brigade"
               }
-            }
+            },
+            {
+              "valueCoding": {"code": "4", "display": "FHIR 🔥 and Ice"}
+            },
+            {
+              "valueCoding": {"code": "5", "display": "FHIR-side chat"}
+            },
+            {
+              "valueCoding": {"code": "6", "display": "FHIR and Rain"}
+            },
+            {
+              "valueCoding": {"code": "7", "display": "Where's the FHIR?"}
+            },
+            {
+              "valueCoding": {"code": "8", "display": "the FHIRing squad"}
+            },
+            {
+              "valueCoding": {"code": "9", "display": "Fight FHIR with FHIR"}
+            },
+            {
+              "valueCoding": {
+                "code": "10",
+                "display": "Call code red, this place is on FHIR"
+              }
+            },
+            {
+              "valueCoding": {"code": "11", "display": "FHIRTruck"}
+            },
+            {
+              "valueCoding": {
+                "code": "12",
+                "display": "Dr. REDSTAT to ED for a FHIR 🔥!"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "13",
+                "display": "I'm the FHIRstarter, twisted FHIRstarter"
+              }
+            },
+            {
+              "valueCoding": {"code": "14", "display": "I got FHIR'd"}
+            },
+            {
+              "valueCoding": {
+                "code": "15",
+                "display": "Liar, Liar, Pants on FHIR"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "16",
+                "display": "Where there's smoke, there's FHIR"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "17",
+                "display": "FHIRing on all cylinders"
+              }
+            },
+            {
+              "valueCoding": {"code": "18", "display": "FHIRwall"}
+            },
+            {
+              "valueCoding": {"code": "19", "display": "Great Balls of FHIR"}
+            },
+            {
+              "valueCoding": {
+                "code": "20",
+                "display": "We don't need no water, let the FHIR burn"
+              }
+            },
+            {
+              "valueCoding": {
+                "code": "21",
+                "display": "We didn't start the FHIR"
+              }
+            },
           ]
         }
       ]
